5a26dc3d4382ec2e6cc88b2d1365aa79b951de7c
Maajid-Ali <maajida2024@gmail.com>
2023-02-01 16:50:57 +0000

    Basket Updated

--- LumaTestingFramework/LumaTestingFramework/Website/Pages/Components/NavBar.cs
+++ LumaTestingFramework/LumaTestingFramework/Website/Pages/Components/NavBar.cs
@@ -7,6 +7,7 @@ public class NavBar
     IWebElement Womens { get; set; }
     IWebElement Mens { get; set; }
 
+
     public NavBar()
     {
         //add code to get the element addresses of the womens and mens links
---
+++ LumaTestingFramework/LumaTestingFramework/Website/Pages/SL_Basket.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using LumaTestingFramework.Website.Pages.Components;
+using SL_TestAutomationFramework;
+
+namespace LumaTestingFramework.Website.Pages
+{
+    public class SL_Basket : SL_StandardPage
+    {
+
+        public SL_Basket(IWebDriver driver) : base (driver)
+        {
+
+        }
+        string _basketUrl = AppConfigReader.Basket;
+
+        private IWebDriver Driver { get; }
+
+        public IWebElement _removeItem1 => Driver.FindElement(By.XPath("//*[@id=\"shopping-cart-table\"]/tbody[1]/tr[2]/td/div/a[2]"));
+        public IWebElement _removeItem2 => Driver.FindElement(By.XPath("//*[@id=\"shopping-cart-table\"]/tbody[2]/tr[2]/td/div/a[2]"));
+     
+        public IWebElement _proceedToCheckout => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/ul/li[1]/button/span"));
+        public IWebElement _totalPrice => Driver.FindElement(By.XPath("//*[@id=\"cart-totals\"]/div/table/tbody/tr[3]/td/strong/span"));
+
+
+        public void RemoveItem1() => _removeItem1.Click();    
+        public void RemoveItem2() => _removeItem2.Click();    
+        public void ProceedToCheckout() => _proceedToCheckout.Click();
+       
+    }
+}
