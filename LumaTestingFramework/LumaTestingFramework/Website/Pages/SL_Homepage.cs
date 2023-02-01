a80ffb28b9d8d17cfd25430784c5349b2ced7d41
Jasser12544 <jasser.bawi19@gmail.com>
2023-02-01 14:35:32 +0000

    Created the homepage POM

--- LumaTestingFramework/LumaTestingFramework/Website/Pages/SL_Homepage.cs
+++ LumaTestingFramework/LumaTestingFramework/Website/Pages/SL_Homepage.cs
@@ -5,15 +5,38 @@ namespace LumaTestingFramework.Website.Pages;
 
 public class SL_Homepage : SL_StandardPage
 {
-    IWebElement ExampleObject;
-    string _homePageUrl = AppConfigReader.BaseUrl;
     public SL_Homepage(IWebDriver driver) : base(driver)
     {
         //Setup IWebElements and custom objects
     }
+     
+    string _homePageUrl = AppConfigReader.BaseUrl;   
+    private IWebDriver Driver { get; }
+    
+    private IWebElement _yogaCollectionRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/a"));
+    private IWebElement _pantsPromotionsRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/div/a[1]"));
+    private IWebElement _teesPromotoinsRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/div/a[2]"));
+    private IWebElement _erinsRecommendationsRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/div/a[3]"));
+    private IWebElement _performanceCollectionsRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/div/a[4]"));
+    private IWebElement _ecoFriendlyCollectionsRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/div/a[5]"));
+    private IWebElement _radiantTeeRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[3]/div/div/ol/li[1]/div/a"));
+    private IWebElement _breateEasyTankRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[3]/div/div/ol/li[2]/div/a"));
+    private IWebElement _argusAllWeatherTankRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[3]/div/div/ol/li[3]/div/a"));
+    private IWebElement _heroHoodieRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[3]/div/div/ol/li[4]/div/a"));
+    private IWebElement _fusionBackpackRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[3]/div/div/ol/li[5]/div/a"));
+    private IWebElement _pushItMessengerBagRedirect => Driver.FindElement(By.XPath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[3]/div/div/ol/li[6]/div/a"));
 
-    public void ExampleAction()
-    {
-        ExampleObject.Click();
-    }
+    public void ClickYogaCollections() => _yogaCollectionRedirect.Click();
+    public void ClickPantsPromotions() => _pantsPromotionsRedirect.Click();
+    public void ClickTeesPromotions() => _teesPromotoinsRedirect.Click();
+    public void ClickErinsPromotions() => _erinsRecommendationsRedirect.Click();
+    public void ClickPerformanceCollections() => _performanceCollectionsRedirect.Click();
+    public void ClickEcoFriendlyCollections() => _ecoFriendlyCollectionsRedirect.Click();
+    public void ClickRadiantTee() => _radiantTeeRedirect.Click();
+    public void ClickBreatheEasyTank() => _breateEasyTankRedirect.Click();
+    public void ClickArgusAllWeatherTank() => _argusAllWeatherTankRedirect.Click();
+    public void ClickHeroHoodie() => _heroHoodieRedirect.Click();
+    public void ClickFusionBackpack() => _fusionBackpackRedirect.Click();
+    public void ClickPushItMessengerBag() => _pushItMessengerBagRedirect.Click();
+
 }
