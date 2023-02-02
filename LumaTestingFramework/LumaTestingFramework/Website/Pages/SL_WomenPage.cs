c87f92d2812257f4fc76ef723be87303cdf67b9b
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-02-02 12:31:25 +0000

    Added properties for elements on the womens page and started writing helper functions

--- LumaTestingFramework/LumaTestingFramework/Website/Pages/SL_WomenPage.cs
+++ LumaTestingFramework/LumaTestingFramework/Website/Pages/SL_WomenPage.cs
@@ -6,17 +6,28 @@ namespace LumaTestingFramework.Website.Pages;
 
 public class SL_WomenPage : SL_StandardPage, INavigate
 {
-    public NavBar NavBar { get; private set; }
     public PageHeader PageHeader { get; private set; }
+    public NavBar NavBar { get; private set; }
+    public List<IWebElement> SideBar { get; private set; }
+    public List<IWebElement> PromoBlock { get; private set; }
+    public List<Product> HotSellers { get; private set; }
 
-    protected SL_WomenPage(IWebDriver driver) : base(driver)
+    public SL_WomenPage(IWebDriver driver) : base(driver)
     {
     }
 
     public void Navigate()
     {
         _driver.Navigate().GoToUrl(AppConfigReader.WomensPage);
-        NavBar = new NavBar(_driver.FindElement(By.TagName("nav")));
+
         PageHeader = new PageHeader(_driver.FindElement(By.CssSelector("header[class='page-header']")));
+        NavBar = new NavBar(_driver.FindElement(By.TagName("nav")));
+        SideBar = _driver.FindElement(By.ClassName("sidebar-main")).FindElements(By.ClassName("item")).ToList();
+        PromoBlock = _driver.FindElement(By.CssSelector("div[class='block-promo']")).FindElements(By.TagName("a")).ToList();
+        HotSellers = Product.ProductsList(_driver);
     }
+
+    public void ClickYogaCollection() => PromoBlock[0].Click();
+    public void ClickWomensTeesPromo() => PromoBlock[1].Click();
+    public void ClickPantsPromo() => PromoBlock[2].Click();
 }
